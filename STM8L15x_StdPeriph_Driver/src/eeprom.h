96cd044da2672c7af070157d8a18e16846350ada
Rob Poliseno <rpoliseno@lutron.com>
2015-02-18 18:12:50 -0500

    Actually followed the directions and got a project that builds. Doesn't seem to run on the board though.

--- STM8L15x_StdPeriph_Driver/src/eeprom.h
+++
@@ -1,7 +0,0 @@
-
-#include "stm8l15x.h"
-
-//prototypes to mkae the compiler happy.
-extern int __eeprom_wait_for_last_operation(void);
-extern void __eeprom_program_byte(unsigned char __near * dst, unsigned char v);
-extern void __eeprom_program_long(unsigned char __near * dst, unsigned long v);
